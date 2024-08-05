1760ef762eb25702bd7735f142565c69ae0b5c6b
yymin1022 <yymin1022@gmail.com>
2024-08-05 11:54:42 +0900

    Update receiver.js : Added Basic Receiver Code

--- src/receiver/receiver.js
+++ src/receiver/receiver.js
@@ -1,1 +1,17 @@
-console.log("Receiver Started!!");
+function initializeReceiver() {
+    const context = cast.framework.CastReceiverContext.getInstance();
+    const playerManager = context.getPlayerManager();
+
+    playerManager.setMessageInterceptor(
+        cast.framework.messages.MessageType.LOAD,
+        request => {
+            console.log('Load request received:', request);
+            return request;
+        }
+    );
+
+    context.start();
+    console.log('Receiver app started');
+}
+
+initializeReceiver();
