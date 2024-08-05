89595ceb368f0db2e0b2f443ced45d7e675d2a76
yymin1022 <yymin1022@gmail.com>
2024-08-05 11:31:12 +0900

    Update index.js : Added Basic HTTP Server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("Hello, World!");
+import fs from 'fs';
+import http from 'http';
+
+const SERVER_PORT = 3000
+
+http.createServer((req, res) => {
+    fs.readFile("./src/index.html", "utf8", (err, data) => {
+        if (err) {
+            res.writeHead(500, {"Content-Type": "text/plain"});
+            res.end("500 Internal Server Error");
+        } else {
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(data);
+        }
+    });
+}).listen(SERVER_PORT, () => {
+    console.log(`Server is Listening at Port ${SERVER_PORT}`);
+});
