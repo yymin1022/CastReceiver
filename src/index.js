c2e1e0a93288e6ba315a8b941ab830e577d38cbe
yymin1022 <yymin1022@gmail.com>
2024-08-05 11:53:51 +0900

    Update index.js : Fixed Filepath Error

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
-import fs from 'fs';
-import http from 'http';
+import fs from "fs";
+import http from "http";
 
-const SERVER_PORT = 3000
+const SERVER_PORT = 3000;
 
 http.createServer((req, res) => {
-    fs.readFile("./src/index.html", "utf8", (err, data) => {
-        if (err) {
-            res.writeHead(500, {"Content-Type": "text/plain"});
-            res.end("500 Internal Server Error");
-        } else {
-            res.writeHead(200, {"Content-Type": "text/html"});
-            res.end(data);
+    let contentType = "text/html";
+    let filePath = "./src/index.html";
+    if (req.url === "/receiver/receiver.js") {
+        contentType = "text/javascript";
+        filePath = "./src/receiver/receiver.js";
+    }
+
+    fs.readFile(filePath, (err, data) => {
+        if(err){
+            if(err.code === "ENOENT"){
+                res.writeHead(404, { "Content-Type": "text/plain" });
+                res.end("404 Not Found");
+            }else{
+                res.writeHead(500, { "Content-Type": "text/plain" });
+                res.end("500 Internal Server Error");
+            }
+        }else{
+            res.writeHead(200, { "Content-Type": contentType });
+            res.end(data, "utf-8");
         }
     });
 }).listen(SERVER_PORT, () => {
-    console.log(`Server is Listening at Port ${SERVER_PORT}`);
+    console.log(`Server is listening on port ${SERVER_PORT}`);
 });
